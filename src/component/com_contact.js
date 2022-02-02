bef57f027d135714a7e8194791a17e78cd98b7b5
zayyid <mochamadzayyid@gmail.com>
2022-02-01 17:36:12 -0800

    contact

--- src/component/com_contact.js
+++ src/component/com_contact.js
@@ -4,6 +4,15 @@ function Contact() {
     return(
         <div className='contact' id='contact'>
             <h1>Contact</h1>
+
+            <div className="contact__container bd-grid">
+                <form action="" className="contact__form">
+                    <input type="text" placeholder="Name" className="contact__input"/>
+                    <input type="mail" placeholder="Email" className="contact__input"/>
+                    <textarea name="" id="" cols="0" rows="10" className="contact__input"></textarea>
+                    <input type="button" value="Send" className="contact_button"/>
+                </form>
+            </div>
         </div>
     );
 }
