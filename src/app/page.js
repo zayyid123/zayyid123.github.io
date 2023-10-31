b961aee01478a5ef33757dbd98245d689dadb0d6
zayyid123 <mochamadzayyid@gmail.com>
2023-10-31 09:56:10 +0700

    add heroicons

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import { BeakerIcon } from '@heroicons/react/24/solid'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>hello</div>
+      <BeakerIcon className="h-6 w-6 text-blue-500" />
     </main>
   )
 }
