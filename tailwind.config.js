ccb728f08e266a63e5c68572c354c6de05742f0c
zayyid123 <mochamadzayyid@gmail.com>
2023-10-31 09:53:16 +0700

    add collor pallet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -12,6 +14,32 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+    },
+    colors: {
+      "def-orange": {
+        100: '#F2A599',
+        200: '#F88F7E',
+        300: '#EF6D58'
+      },
+      "def-purple": {
+        100: '#D193F9',
+        200: '#C06BF9',
+        300: '#B550F8'
+      },
+      "def-yellow": {
+        100: '#F9E4A2',
+        200: '#F9D76C',
+        300: '#F7CC47'
+      },
+      "def-white": {
+        100: '#b1b2b9',
+        200: '#FFF',
+      },
+      "bg": {
+        100: '#28293e',
+        200: '#fdf0e9',
+      },
+      ...colors,
     },
   },
   plugins: [],
